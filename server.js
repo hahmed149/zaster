aded868c88b2a1fdc858a2bd7a09a6c070f09786
Hamza Ahmed <hahmed149@outlook.com>
2018-10-20 21:29:46 -0500

    Vision api working and posting data to info.ejs

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const app = express();
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
-
-
+const vision = require('./vision');
+let urlForImag;
 const nasa_api = process.env.NASA_API;
 const urlImageOfDay = 'https://api.nasa.gov/planetary/apod?api_key='+nasa_api;
 
 app.get('/', function(req, res) {
   request(urlImageOfDay, function(error, response, body) {
      let data = JSON.parse(body).hdurl;
-     console.log(data);
+     
+     urlForImag = data;
+     console.log(urlForImag);
     res.render('index', {data});
  
   });
@@ -36,9 +38,26 @@ app.get('/iss', function(req, res) {
  
   });
 });
-
-
 //end api for iss
+
+
+//start vision api**************************
+
+app.get('/info', function(req, res) {
+  vision.visionEngine(urlForImag, function(labels) {
+    console.log(labels);
+    res.render('info', {labels});
+
+  });
+});
+
+//end vision api
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
--- vision.js
+++ vision.js
@@ -1,3 +1,6 @@
+module.exports = {
+visionEngine : function(urlImg, callback)
+{
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
@@ -12,15 +15,22 @@ const client = new vision.ImageAnnotatorClient({
     projectId: projectId,
   });
 
+
+
+
 // Performs label detection on the image file
 client
-  .labelDetection('C:\\Users\\Abdul\\Desktop\\download.jpg')
+  .labelDetection('https://apod.nasa.gov/apod/image/1810/NGC6543-BYU-L.jpg')
   .then(results => {
     const labels = results[0].labelAnnotations;
 
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
+      callback(labels) ;
   })
   .catch(err => {
     console.error('ERROR:', err);
+    return null;
   });
+}
+}
